5394a582361ec84ddf2a19ca946e32389be1acc6
xhitz <heylistenplz@gmail.com>
2021-01-14 13:54:37 -0800

    Changed Title Names

--- my_functions/animal.js
+++ my_functions/animal.js
@@ -6,13 +6,13 @@ exports.handler = async function (event, context) {
       headers: { 'Cache-Control': 'max-age=10' },
       body: JSON.stringify({ name: 'Drops', species: 'cat', photo: 'https://learnwebcode.github.io/json-example/images/cat-1.jpg', bio: 'This cat is great and very vocal. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque.' })
     }
-  } else if (name === 'barksalot') {
+  } else if (name === 'Marketplace') {
     return {
       statusCode: 200,
       headers: { 'Cache-Control': 'max-age=10' },
       body: JSON.stringify({ name: 'Barksalot', species: 'dog', photo: 'https://learnwebcode.github.io/json-example/images/dog-1.jpg', bio: 'This dog is very communicative. Deleniti, tempora quis commodi qui inventore ratione rem porro doloribus et obcaecati cumque quibusdam voluptatibus iure nisi aut minima consequuntur, officiis esse? Lorem ipsum, dolor sit amet consectetur adipisicing elit.' })
     }
-  } else if (name === 'purrsloud') {
+  } else if (name === 'Designers') {
     return {
       statusCode: 200,
       headers: { 'Cache-Control': 'max-age=10' },
