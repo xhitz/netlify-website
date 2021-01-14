5af79c20ad7cb8ec55483bc3111a8e6eaa3c7b27
xhitz <heylistenplz@gmail.com>
2021-01-14 13:55:09 -0800

    Changed Titles

--- frontend-src/App.js
+++ frontend-src/App.js
@@ -1,35 +1,35 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Link, NavLink, Switch } from "react-router-dom"
-import Home from "./components/Home"
-import Animal from "./components/Animal"
-import About from "./components/About"
-import Secret from "./components/Secret"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Animal from './components/Animal'
+import About from './components/About'
+import Secret from './components/Secret'
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
-      <header className="bg-light pt-3">
-        <div className="container">
-          <h1 className="display-1">Our Amazing App</h1>
-          <ul className="nav nav-tabs">
-            <li className="nav-item">
-              <NavLink to="/animal/meowsalot" className="nav-link" activeClassName="active">
-                Meowsalot
+      <header className='bg-light pt-3'>
+        <div className='container'>
+          <h1 className='display-1'>SydTek</h1>
+          <ul className='nav nav-tabs'>
+            <li className='nav-item'>
+              <NavLink to='/animal/Drops' className='nav-link' activeClassName='active'>
+                Drops
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/animal/barksalot" className="nav-link" activeClassName="active">
+            <li className='nav-item'>
+              <NavLink to='/animal/Marketplace' className='nav-link' activeClassName='active'>
                 Barksalot
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/animal/purrsloud" className="nav-link" activeClassName="active">
+            <li className='nav-item'>
+              <NavLink to='/animal/Designers' className='nav-link' activeClassName='active'>
                 Purrsloud
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/secret" className="nav-link" activeClassName="active">
+            <li className='nav-item'>
+              <NavLink to='/secret' className='nav-link' activeClassName='active'>
                 Secret
               </NavLink>
             </li>
@@ -37,30 +37,30 @@ function App() {
         </div>
       </header>
 
-      <div className="container py-3 py-md-5">
+      <div className='container py-3 py-md-5'>
         <Switch>
-          <Route path="/animal/:name">
+          <Route path='/animal/:name'>
             <Animal />
           </Route>
-          <Route path="/secret">
+          <Route path='/secret'>
             <Secret />
           </Route>
-          <Route path="/about">
+          <Route path='/about'>
             <About />
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <Home />
           </Route>
         </Switch>
       </div>
 
-      <footer className="border-top pt-3">
-        <p className="small text-center text-muted">
-          <Link className="text-muted mr-1" to="/">
+      <footer className='border-top pt-3'>
+        <p className='small text-center text-muted'>
+          <Link className='text-muted mr-1' to='/'>
             Home
-          </Link>{" "}
-          |{" "}
-          <Link className="text-muted ml-1" to="/about">
+          </Link>{' '}
+          |{' '}
+          <Link className='text-muted ml-1' to='/about'>
             About Us
           </Link>
         </p>
@@ -69,4 +69,4 @@ function App() {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById('app'))
