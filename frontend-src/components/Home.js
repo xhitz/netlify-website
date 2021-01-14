de4d4f473e170e44c3ffaf090ec2c127d02cd0e0
xhitz <heylistenplz@gmail.com>
2021-01-14 14:09:39 -0800

    Added placeholder

--- frontend-src/components/Home.js
+++ frontend-src/components/Home.js
@@ -7,9 +7,9 @@ function Home () {
 
   return (
     <div>
-      <h2 className='display-4'>Welcome to the homepage!</h2>
-      <p className='lead'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quasi natus fugit ab laudantium dolores, dicta blanditiis sit eum perferendis minima sunt repudiandae voluptates tempore. Fuga dicta ipsa beatae vel?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error eveniet dignissimos alias repellat officia qui rem nemo, unde magnam voluptatum obcaecati explicabo tempora quidem? Aperiam doloremque assumenda accusantium deleniti soluta?</p>
+      <h2 className='display-4'>Sale your items on the blockchain network with SydTek </h2>
+      <p className='lead'>List of items autoscrolling right here</p>
+      <p>List some the services our site has here</p>
     </div>
   )
 }
